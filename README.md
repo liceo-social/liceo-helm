319bece7b64fafedee3d51c67254891c535111fd
Mario Garcia <mario.ggar@gmail.com>
2020-12-13 20:08:21 +0100

    chores(init): commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+## Liceo Helm
+
+Este repositorio contiene un chart de Helm para poder desplegar Liceo en un entorno de Kubernetes. Si quieres saber mas sobre como instalar Liceo o como usar Liceo, echale un vistazo a la documentacion.
+
+- [Instalacion de Liceo](https://liceo-social.github.io/guide/)
+- [Guia de uso de Liceo](https://liceo-social.github.io/install/)
---
+++ liceo.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+set -e 
+
+function redeploy {
+    if [ -n "$1" ]; then
+        kubectl rollout restart deployment/$1
+    else
+        echo "liceo.sh redeploy liceo-web|liceo-db"
+    fi
+}
+
+function log {
+    echo "liceo.sh: $1"
+}
+
+function list-deployments {
+    kubectl get deployments
+}
+
+function backup-files {
+    BACKUP_FILENAME="liceo-fs-$(date +%Y%m%d%H%M).tar.gz"
+
+    log "(backup-files) getting liceo-web pod name"
+    POD_WEB_NAME=$(kubectl get pods -l tier=web | tail -1 | awk '{print $1}')
+
+    log "(backup-files) creating tarball file"
+    kubectl exec $POD_WEB_NAME -- tar czf $BACKUP_FILENAME /tmp/
+
+    log "(backup-files) copying tarball file to local"
+    kubectl cp socseross/$POD_WEB_NAME:$BACKUP_FILENAME $BACKUP_FILENAME
+}
+
+function backup-db {
+    BACKUP_FILENAME="liceo-db-$(date +%Y%m%d%H%M).sql"
+
+    log "(backup-db) getting liceo-db pod name"
+    POD_DB_NAME=$(kubectl get pods -l tier=db | tail -1 | awk '{print $1}')
+
+    log "(backup-db) executing db dump"
+    kubectl exec $POD_DB_NAME -- pg_dump -U liceo -f /tmp/$BACKUP_FILENAME
+
+    log "(backup-db) copying dump file to local"
+    kubectl cp socseross/$POD_DB_NAME:/tmp/$BACKUP_FILENAME $BACKUP_FILENAME
+}
+
+function backup-all {
+    backup-files
+    backup-db
+}
+
+function usage {
+    echo "liceo.sh redeploy|list|backup-all|backup-db|backup-files"
+}
+
+case $1 in 
+    redeploy)
+        redeploy $2
+        ;;
+    list)
+        list-deployments
+        ;;
+    backup-all)
+        backup-all
+        ;;
+    backup-db)
+        backup-db
+        ;;
+    backup-files)
+        backup-files
+        ;;
+    *)
+    usage
+    ;;
+esac
